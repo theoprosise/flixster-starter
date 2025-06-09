8bc8decd40c456dd2111d899816e3d94957d4c07
Theo Martin Prosise <theoprosise@meta.com>
2025-06-09 13:11:21 -0700

    Implement load more feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import MovieList from '../MovieList'
+import MovieList from './components/MovieList'
+import SearchForm from './components/SearchForm'
+
 
 const App = () => {
 
 let [data, setMovieData] = useState([]);
+let [page, setPage] = useState(1);
+let [searchQuery, setSearchQuery] = useState("");
 
 useEffect(() => {
-  const fetchNowPlaying = async () => {
+  fetchNowPlaying();
+}, [page])
+
+const FetchSearch = async() =>{
     const accessToken = import.meta.env.VITE_ACCESS_TOKEN;
-    const url = "https://api.themoviedb.org/3/movie/now_playing";
+    const url = `https://api.themoviedb.org/3/search/movie?query=${searchQuery}`;
     
     try{
       const res = await fetch(url, {
@@ -24,28 +31,63 @@ useEffect(() => {
         console.log("Error - check res status code")
       }
 
-      const data = await res.json();
-      setMovieData(data.results);
+      const searchData = await res.json();
+      setMovieData(searchData.results);
 
     }
     catch(err){
       console.log("ERROR - check err");
     }
+}
 
+
+const fetchNowPlaying = async () => {
+    const accessToken = import.meta.env.VITE_ACCESS_TOKEN;
+    const url = `https://api.themoviedb.org/3/movie/now_playing?&page=${page}`;
+    
+    try{
+      const res = await fetch(url, {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      if(!res.ok){
+        console.log("Error - check res status code")
+      }
+
+      const newData = await res.json();
+      setMovieData([...data,...newData.results]);
+
+    }
+    catch(err){
+      console.log("ERROR - check err");
+    }
   };
 
+
+useEffect(() => {
   fetchNowPlaying();
 },[]);
 
-
+function handleSearchChange(query){
+  setSearchQuery(query);
+}
 
   return (
     <div className="App">
-    <header className='header'>Flixter</header>
+    <header className='header'>Flixter
+      <SearchForm className="searchForm" onSearchChange={handleSearchChange}/>
+    </header>
     <main>
       {/*Populate MovieList with MovieCards and data from API*/}
       
       <MovieList data={data}/>
+      <button onClick={() => setPage(page => page +1)}>
+        Load More
+      </button>
     </main>
     </div>
   );
